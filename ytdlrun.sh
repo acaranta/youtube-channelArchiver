d4101924e4e033384eebd4d2543a0c6b96467d7c
acaranta <arthur@caranta.com>
2020-05-23 16:35:35 +0200

    Added OPTIONS env

--- ytdlrun.sh
+++ ytdlrun.sh
@@ -8,5 +8,5 @@ then
 	COOKIES="--cookies $CONFIGDIR/cookies.txt"
 fi
 
-youtube-dl --playlist-reverse --download-archive $CONFIGDIR/downloaded.txt -i -o "/ytdl/media/%(playlist)s/%(playlist)s - S01E%(playlist_index)s - %(uploader)s - %(title)s [%(id)s].%(ext)s" -f bestvideo[ext=mp4]+bestaudio[ext=m4a] --merge-output-format mp4 --add-metadata --write-thumbnail --batch-file=$CONFIGDIR/channel_list.txt $COOKIES
+youtube-dl $YTOPTIONS --playlist-reverse --download-archive $CONFIGDIR/downloaded.txt -i -o "/ytdl/media/%(playlist)s/%(playlist)s - S01E%(playlist_index)s - %(uploader)s - %(title)s [%(id)s].%(ext)s" -f bestvideo[ext=mp4]+bestaudio[ext=m4a] --merge-output-format mp4 --add-metadata --write-thumbnail --batch-file=$CONFIGDIR/channel_list.txt $COOKIES
 #youtube-dl --playlist-reverse --download-archive $CONFIGDIR/downloaded.txt -i -o "/ytdl/media/%(uploader)s/%(playlist)s/%(playlist)s - S01E%(playlist_index)s - %(title)s [%(id)s].%(ext)s" -f bestvideo[ext=mp4]+bestaudio[ext=m4a] --merge-output-format mp4 --add-metadata --write-thumbnail --batch-file=$CONFIGDIR/channel_list.txt $COOKIES
